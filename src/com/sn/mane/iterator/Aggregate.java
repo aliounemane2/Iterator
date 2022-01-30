d2d4457b10a1d8258b24ff973c2a43490104bd8f
Alioune MANE <aliounemane@finetechgroupe.com>
2022-01-30 18:03:19 +0000

    Design Pattern Iterator

---
+++ src/com/sn/mane/iterator/Aggregate.java
@@ -0,0 +1,12 @@
+package com.sn.mane.iterator;
+
+/**
+ *
+ * Aggregate defines an interface for creating an Iterator object.
+ *
+ */
+public interface Aggregate {
+
+    Iterator createIterator();
+
+}
